cc587ed7453de866d83e00fc02b2887b035bcd01
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-04-24 10:41:58 -0400

    Day 28 Class

---
+++ src/day28_DateTime/Time.java
@@ -0,0 +1,18 @@
+package day28_DateTime;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Time {
+    public static void main(String[] args) {
+
+        LocalTime time1 = LocalTime.now();
+        System.out.println(time1);
+
+        LocalTime time2 = LocalTime.of(15, 30, 0);
+        System.out.println(time2);
+
+        DateTimeFormatter
+
+    }
+}
