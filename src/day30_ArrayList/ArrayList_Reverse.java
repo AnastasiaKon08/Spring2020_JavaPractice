52845bbe07c07b30d51561d88d4c274a447e0135
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-04-28 10:50:32 -0400

    Day30

---
+++ src/day30_ArrayList/ArrayList_Reverse.java
@@ -0,0 +1,32 @@
+package day30_ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList_Reverse {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=1; i<6; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+
+        ArrayList<Integer> reverseList = new ArrayList<>();
+
+
+        for(int i =list.size()-1; i>=0; i--){
+            int eachNum = list.get(i); // unboxing
+            reverseList.add(eachNum);
+            System.out.print(eachNum+" ");
+        }
+
+        System.out.println("\n"+ reverseList);
+    }
+}
+
+
+/*
+ write a program that can print the list of integers in reversed order
+		ex:
+			list=> {1,2,3,4,5}
+			output: 5 4 3 2 1
+ */
