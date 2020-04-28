9174d8a6d9af63a7cad8c269744e74de37c6968d
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-04-28 12:40:02 -0400

    d30

--- src/day30_ArrayList/ArrayList_Methods.java
+++ src/day30_ArrayList/ArrayList_Methods.java
@@ -13,5 +13,24 @@ public class ArrayList_Methods {
         System.out.println(list);
         list.set(3, 66);
         System.out.println(list);
+
+        System.out.println("====================================");
+        ArrayList <Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(3);
+        list3.add(4);
+
+        Integer a= 1;
+        list3.remove(a);
+        System.out.println(list3);
+
+        ArrayList <Integer> list4= new ArrayList<>();
+        list4.add(10);
+        list4.add(20);
+        list4.add(30);
+        Integer b = 20;
+        boolean r1 = list4.remove(b);
+        System.out.println(r1);
     }
 }
