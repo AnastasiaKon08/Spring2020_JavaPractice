56475eca3bf3f871c5845c0c9e10dd9d1c5cc155
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-05-29 11:47:12 -0400

    ExceptionKeyword

---
+++ src/day45_Exceptions/ThrowKeyword.java
@@ -0,0 +1,14 @@
+package day45_Exceptions;
+
+import java.util.NoSuchElementException;
+
+public class ThrowKeyword {
+
+    public static void main(String[] args) {
+       NoSuchElementException obj =  new NoSuchElementException();
+
+       // throw obj;
+
+       throw new NoSuchElementException();
+    }
+}
