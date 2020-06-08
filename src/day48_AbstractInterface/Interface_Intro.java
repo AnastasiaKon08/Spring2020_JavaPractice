91c3e532690f5242ead5e83585e7fd09149b8320
Anastasia Kononosova <mrs.anastasia96@gmail.com>
2020-06-08 11:58:39 -0400

    Interface

--- src/day48_AbstractInterface/Interface_Intro.java
+++ src/day48_AbstractInterface/Interface_Intro.java
@@ -2,4 +2,9 @@ package day48_AbstractInterface;
 
 public interface Interface_Intro {
 
+        public abstract void method1();
+        public void method2(); // abstract by default
+       // void method3(); // access modifier public by default
+
+
 }
---
+++ src/day48_AbstractInterface/Test.java
@@ -0,0 +1,12 @@
+package day48_AbstractInterface;
+
+public class Test implements Interface_Intro {
+
+    @Override
+    public void method1(){
+    }
+
+    @Override
+    public void method2(){}
+
+}
